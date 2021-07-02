8830dc4e8dc6db84f492dd38f2c14a31335ca670
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2021-07-03 02:33:56 +0430

    feat (bubble-sort): implemented with PHP

---
+++ bubble-sort/bubble-sort.php
@@ -0,0 +1,48 @@
+<?php
+
+function getRandomArray($len)
+{
+    $tmpArr = range(0, $len - 1);
+    shuffle($tmpArr);
+    return $tmpArr;
+}
+
+function shouldSwap($a, $b, $dir)
+{
+    return $dir === "ASC" ? $a > $b : $a < $b;
+}
+
+function swap($keyA, $keyB, $arr)
+{
+    $tmpArr = array_replace([], $arr);
+    [$tmpArr[$keyA], $tmpArr[$keyB]] = [$tmpArr[$keyB], $tmpArr[$keyA]];
+
+    return $tmpArr;
+}
+
+function bubbleSort($arr, $dir)
+{
+    $cpyArr = array_replace([], $arr);
+    $arrLength = count($cpyArr);
+
+    for ($x = 0; $x < $arrLength; $x++)
+        for ($y = 0; $y < ($arrLength - 1 - $x); $y++) {
+            $currentVal = $cpyArr[$y];
+            $nextVal = $cpyArr[$y + 1];
+            if (shouldSwap($currentVal, $nextVal, $dir)) {
+                $cpyArr = swap($y, $y + 1, $cpyArr);
+            }
+        }
+
+    return $cpyArr;
+}
+
+$testArray = getRandomArray(20);
+
+echo "Input:\n";
+print_r($testArray = getRandomArray(20));
+
+echo "\n";
+
+echo "Output:\n";
+print_r(bubbleSort($testArray, "ASC"));
