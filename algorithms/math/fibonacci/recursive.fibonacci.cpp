5596060c537b87abdb4c5da95ba253f89c765578
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-03-21 18:55:41 +0330

    (fibonacci): implemented recursively with CPP

---
+++ algorithms/math/fibonacci/recursive.fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int
+fib(int n)
+{
+  if (n <= 1)
+    return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+int
+main()
+{
+  int n = 40;
+
+  cout << fib(n) << endl;
+}
